3a47bd3c9916c282f079e587947a39d7eb957198
Shubham Gaikwad <shubhamgaikwad3297@gmail.com>
2025-09-10 20:52:19 +0530

    Added favicon and website name

--- script.js
+++ script.js
@@ -15,42 +15,46 @@
 //   }
 // }, 35);
 
-let tl = gsap.timeline();
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.2,
-  duration: 0.6,
-  delay: 0.5,
-});
-tl.from("#line1-part1", {
-  opacity: 0,
-  onStart: function () {
-    let h5Timer = document.querySelector("#line1-part1 h5");
-    let grow = 0;
-    setInterval(() => {
-      if (grow < 100) {
-        h5Timer.innerHTML = grow++;
-      } else {
-        h5Timer.innerHTML = grow;
-      }
-    }, 35);
-  },
-});
-tl.to(" .line h2", {
-  animationName: "anime",
-  opacity: 1,
-});
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.2,
-  delay: 4,
-});
-tl.from("#page1", {
-  y: 1600,
-  opacity: 0,
-  delay: 0.5,
-  ease: Power4, // Check after
-});
-tl.to("#loader", {
-  display: "none",
-});
+function loadingAnimation() {
+  let tl = gsap.timeline();
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.2,
+    duration: 0.6,
+    delay: 0.5,
+  });
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function () {
+      let h5Timer = document.querySelector("#line1-part1 h5");
+      let grow = 0;
+      setInterval(() => {
+        if (grow < 100) {
+          h5Timer.innerHTML = grow++;
+        } else {
+          h5Timer.innerHTML = grow;
+        }
+      }, 35);
+    },
+  });
+  tl.to(" .line h2", {
+    animationName: "anime",
+    opacity: 1,
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 4,
+  });
+  tl.from("#page1", {
+    y: 1600,
+    opacity: 0,
+    delay: 0.5,
+    ease: Power4, // Check after
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+
+loadingAnimation();
